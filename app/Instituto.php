a8801f7967c36d02136b02a72b2da2f7b943f50a
Ronny Arvelo <ronny.arvelo@gmail.com>
2016-03-22 21:12:35 -0430

    working on institutos

--- app/Instituto.php
+++ app/Instituto.php
@@ -8,7 +8,7 @@ class Instituto extends Model
 {
     //
     protected $table = 'institutos';
-    protected $fillable = ['name', 'desc_es', 'desc_en', 'logo', 'location1', 'coord1', 'location2', 'coord2', 'location3', 'coord3', 'reasons_es', 'reasons_en', 'website', 'tel', 'campusttour_es', 'campustour_en', 'programs_es', 'programs_en', 'accomm_es', 'accomm_en', 'activities_es', 'activities_en', 'price', 'mail',];
+    protected $fillable = ['name', 'desc_es', 'desc_en', 'logo', 'location1', 'coord1', 'location2', 'coord2', 'location3', 'coord3', 'reasons_es', 'reasons_en', 'website', 'tel', 'campustour_es', 'campustour_en', 'programs_es', 'programs_en', 'accomm_es', 'accomm_en', 'activities_es', 'activities_en', 'price', 'mail',];
 
     public function programas()
     {

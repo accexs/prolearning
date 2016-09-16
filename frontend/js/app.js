a39256610d89be32ed4dd099a096ca7bfff671b0
Ronny Arvelo <ronny.arvelo@gmail.com>
2016-09-16 18:15:30 -0300

    angular ui-router

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,15 +2,14 @@ var frontPl = angular.module('frontPl',
 	[
 	'ui.router',
 	'ngAnimate'
-	])
+	]);
 
 frontPl.config(function($stateProvider, $urlRouterProvider) {
     
     $urlRouterProvider.otherwise('/home');
-    
     $stateProvider.state('home', {
             url: '/home',
-            templateUrl: '_partials/_home.html'
+            templateUrl: './_partials/_home.html'
         });
         
         

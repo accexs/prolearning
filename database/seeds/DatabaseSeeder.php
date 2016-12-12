d78ab1554d46ee21c05d7ab9717cf1b09b60a711
Ronny Arvelo <ronny.arvelo@gmail.com>
2016-12-12 05:48:49 -0300

    fix seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
     	$this->call(TiposTableSeeder::class);
-    	$this->call(UsersTableseeder::class);
+    	$this->call(UsersTableSeeder::class);
     }
 }

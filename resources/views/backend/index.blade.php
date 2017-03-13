b71d971a25a30c7311194f7107dc0cc528a691c2
Ronny Arvelo <ronny.arvelo@gmail.com>
2017-03-13 02:53:41 -0300

    work on front end

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -2,4 +2,76 @@
 
 @section('content')
 
+<div class="container">
+	<div>
+		<br><br>
+	</div>
+	<div>
+		<h1>Panel administrativo</h1>
+	</div>
+	<div>
+		<br><br>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="panel panel-primary">
+				<a class='whole-div-link' href={{ url('/ciudad') }}></a>
+				<div class="panel-heading">
+					<h3 class="panel-title">Paises y ciudades</h3>
+				</div>
+				<div class="panel-body">
+					Panel content
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-primary">
+				<a class='whole-div-link' href={{ url('/instituto') }}></a>
+				<div class="panel-heading">
+					<h3 class="panel-title">Institutos</h3>
+				</div>
+				<div class="panel-body">
+					Panel content
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-primary">
+				<a class='whole-div-link' href={{ url('/programa') }}></a>
+				<div class="panel-heading">
+					<h3 class="panel-title">Programas y cursos</h3>
+				</div>
+				<div class="panel-body">
+					Panel content
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="panel panel-primary">
+				<a class='whole-div-link' href={{ url('/promo') }}></a>
+				<div class="panel-heading">
+					<h3 class="panel-title">Promociones</h3>
+				</div>
+				<div class="panel-body">
+					Panel content
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-primary">
+				<a class='whole-div-link' href={{ url('/testimonio') }}></a>
+				<div class="panel-heading">
+					<h3 class="panel-title">Testimonios</h3>
+				</div>
+				<div class="panel-body">
+					Panel content
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
 @endsection
